064e359936c7f79ebfdbadf62c27e0bc4817e36d
starksy14 <starksy@gmail.com>
2024-01-06 17:32:01 -0600

    modified:   src/pages/GameOneTable.js

--- src/pages/GameOneTable.js
+++ src/pages/GameOneTable.js
@@ -26,17 +26,41 @@ export default function GameOneTable() {
           <tr>
               <td>Stephen F. Austin</td>
               <td>{scheduleUCLA.map((scheduleUCLA) => scheduleUCLA.UCLATeam12)}</td>
+              <td>Hawaii</td>
+              <td>{scheduleUCLA.map((scheduleUCLA) => scheduleUCLA.UCLATeam11)}</td>
               <td>Female</td>
-            <td>Hawaii</td>
-            <td>{scheduleUCLA.map((scheduleUCLA) => scheduleUCLA.UCLATeam11)}</td>
               <td>Female</td>
           </tr>
           <tr>
               <td>Abilene Christian</td>
               <td>{scheduleUCLA.map((scheduleUCLA) => scheduleUCLA.UCLATeam16)}</td>
-              <td>Male</td>
             <td>Northern Illinois</td>
             <td>{scheduleUCLA.map((scheduleUCLA) => scheduleUCLA.UCLATeam15)}</td>
+            <td>Male</td>
+              <td>Male</td>
+          </tr>
+          <tr>
+          <td>{scheduleUCLA.map((scheduleUCLA) => scheduleUCLA.UCLATeam21)}</td>
+              <td>@Bryant</td>
+              <td>{scheduleUCLA.map((scheduleUCLA) => scheduleUCLA.UCLATeam20)}</td>
+            <td>@Ball State</td>
+              <td>19</td>
+              <td>Male</td>
+          </tr>
+          <tr>
+          <td>{scheduleUCLA.map((scheduleUCLA) => scheduleUCLA.UCLATeam25)}</td>
+              <td>@Nicholls State</td>
+            <td>Lamar</td>
+            <td>{scheduleUCLA.map((scheduleUCLA) => scheduleUCLA.UCLATeam13)}</td>
+            <td>Female</td>
+              <td>Female</td>
+          </tr>
+          <tr>
+              <td>Buffalo</td>
+              <td>{scheduleUCLA.map((scheduleUCLA) => scheduleUCLA.UCLATeam8)}</td>
+              <td>{scheduleUCLA.map((scheduleUCLA) => scheduleUCLA.UCLATeam7)}</td>
+            <td>@Houston</td>
+              <td>Male</td>
               <td>Male</td>
           </tr>
       </table>
