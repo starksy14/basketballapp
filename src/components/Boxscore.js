f9dc9cd1ed100efc7969dbe401c37a2d726fd9ef
starksy14 <starksy@gmail.com>
2024-01-13 11:25:23 -0600

    modified:   src/components/Boxscore.js 	modified:   src/components/Purdue.js 	modified:   src/components/PurdueNames.js

--- src/components/Boxscore.js
+++ src/components/Boxscore.js
@@ -1,4 +1,5 @@
-import { PurduePlayers } from "./PurdueNames";
+import { generatePlayerNames } from './PurdueNames';
+import { YourComponent } from './PurdueNames';
 import { TexasPlayers } from "./Texas"
 
 
@@ -86,16 +87,7 @@ export default function PurdueTexasBox() {
         <div className="row">
           <div className="col-sm">
           <h4>Purdue</h4>
-          <h2>{PurduePlayers.map((PurduePlayers) => PurduePlayers.purduename1)}</h2>
-          <h2>{PurduePlayers.map((PurduePlayers) => PurduePlayers.purduename2)}</h2>
-          <h2>{PurduePlayers.map((PurduePlayers) => PurduePlayers.purduename3)}</h2>
-          <h2>{PurduePlayers.map((PurduePlayers) => PurduePlayers.purduename4)}</h2>
-          <h2>{PurduePlayers.map((PurduePlayers) => PurduePlayers.purduename5)}</h2>
-          <h2>{PurduePlayers.map((PurduePlayers) => PurduePlayers.purduename6)}</h2>
-          <h2>{PurduePlayers.map((PurduePlayers) => PurduePlayers.purduename7)}</h2>
-          <h2>{PurduePlayers.map((PurduePlayers) => PurduePlayers.purduename8)}</h2>
-          <h2>{PurduePlayers.map((PurduePlayers) => PurduePlayers.purduename9)}</h2>
-          <h2>{PurduePlayers.map((PurduePlayers) => PurduePlayers.purduename10)}</h2>
+          <h2><YourComponent generatePlayerNames={generatePlayerNames} /></h2>
             </div>
         </div>
         <div className="row">
